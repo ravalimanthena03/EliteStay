bbc7481c0e569bd93290fa0b34af51ad4c96ed1f
ravalimanthena03 <119487164+ravalimanthena03@users.noreply.github.com>
2024-12-08 19:56:54 +0530

    Delete Backend directory

--- Backend/Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HotelManagementSysMongoDB.Models
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-    }
-}
